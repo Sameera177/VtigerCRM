f238e534a4f8dbf29575b23dd10f0dd231c23127
Sameera <shanurvm@gmail.com>
2024-11-28 10:07:26 +0530

    FitPeo Assignment

--- src/test/java/fitpeoScript/FitPeoAssignment.java
+++ src/test/java/fitpeoScript/FitPeoAssignment.java
@@ -17,7 +17,7 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 //This is an interview script
-public class TestScript 
+public class FitPeoAssignment 
 {
 	@Test
 	public void fitPeoTest() throws InterruptedException, AWTException
